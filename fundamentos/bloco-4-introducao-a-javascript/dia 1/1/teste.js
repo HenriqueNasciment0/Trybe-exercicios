de5b8cfbb8da467c55c3b8d9b707664faddcea7b
Henrique Nascimento <hsncorretor@gmail.com>
2021-11-28 19:25:26 -0300

    adiciona exercicios 1 e 2 do dia

---
+++ fundamentos/bloco-4-introducao-a-javascript/dia 1/1/teste.js
@@ -0,0 +1,12 @@
+let notaMaria = 10;
+
+if (notaMaria >= 80) {
+    console.log("Parabéns, você foi aprovada!");
+}
+else if (notaMaria >= 60 && notaMaria < 80) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada");
+}
+
