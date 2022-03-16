74145b9420c82c03ae45b1b787539febe03ffb23
HenriqueNasciment0 <hsncorretor@gmail.com>
2022-03-16 16:05:46 -0300

    iniciando exercicios de test

---
+++ front-end/bloco-14-Testes-automatizados-RTL/testes-react/src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/olha o bug aí/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('verifica se exite um input "email"', () => {
+  render(<App />);
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
