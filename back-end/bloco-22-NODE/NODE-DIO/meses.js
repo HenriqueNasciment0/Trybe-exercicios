3e65c11891950f26b2c50bbb315c395b94dd36a2
HenriqueNasciment0 <henrique@HENRIQUE-ACER.localdomain>
2022-06-13 19:17:27 -0300

    apenas o início

---
+++ back-end/bloco-22-NODE/NODE-DIO/meses.js
@@ -0,0 +1,14 @@
+let months = {
+    "1": "January",
+    "2": "February",
+    "4": "Aplil",
+    
+  //TODO: Complete os espaços em branco com uma possível solução para o desafio
+  
+  };
+  
+  function howMonth (num) {
+      return months[num];
+    }
+  
+  console.log(howMonth(1));
