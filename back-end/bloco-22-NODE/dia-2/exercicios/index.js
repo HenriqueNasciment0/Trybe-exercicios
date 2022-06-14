ced0f904400a9b719cab430a82630205ae04bdb9
HenriqueNasciment0 <henrique@HENRIQUE-ACER.localdomain>
2022-06-14 20:56:54 -0300

    exercicio simpsons 2

--- back-end/bloco-22-NODE/dia-2/exercicios/index.js
+++ back-end/bloco-22-NODE/dia-2/exercicios/index.js
@@ -12,3 +12,23 @@ const findSimpson = async () => {
 }
 
 findSimpson();
+
+//Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com 
+//os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado,
+//rejeite a Promise com o motivo "id não encontrado".
+
+const findSimpsonById = async (id) => {
+    const file = await fs.readFile('./simpsons.json', 'utf-8');
+    const simpsons = JSON.parse(file);
+    const print = simpsons.find((e) => Number(e.id) === id);
+    if(!print) {
+        throw new Error('id não encontrado');
+    }
+    return print;
+}
+
+const main = async () => {
+    const simpsons = await findSimpsonById(2);
+    console.log("ex. 2: ", simpsons);
+}
+main();
