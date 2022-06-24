ec3a953b68548a07f7d34bdc2800096a4ab13d28
HenriqueNasciment0 <henrique@HENRIQUE-ACER.localdomain>
2022-06-24 14:46:50 -0300

    começando vamos praticar2

--- back-end/bloco-23-MSC/dia-1/vamos-praticar-1/index.js
+++ back-end/bloco-23-MSC/dia-1/vamos-praticar-1/index.js
@@ -19,6 +19,17 @@ app.get('/books', async (req, res) => {
 	res.status(200).json(books);
 })
 
+// app.get('/books/:id', async (req, res) => {
+// 	const { id } = req.params;
+// 	const booksById = await Books.getByAuthorId(Number(id));
+
+// 	if (!id) {
+// 		return res.status(400).json({ message: 'Not found' });
+// 	}
+	
+// 	res.status(200).json(booksById);
+// })
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
