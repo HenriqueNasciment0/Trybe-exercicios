110a1160a7813cbcc20adb85f263ecdd2d89134b
HenriqueNasciment0 <you@example.com>
2022-07-08 12:51:26 -0300

    feat: cria camada controler e rota no index

--- back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/index.js
+++ back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const bookController = require('./controllers/bookController');
 
 app.use(express.json());
+
+app.get('/books', bookController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
