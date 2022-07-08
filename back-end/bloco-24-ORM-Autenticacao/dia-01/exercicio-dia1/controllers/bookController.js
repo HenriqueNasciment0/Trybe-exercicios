9635e61e57e9fce6dd60b4e1426047df5d10aca4
HenriqueNasciment0 <you@example.com>
2022-07-08 14:58:57 -0300

    feat: adiciona variáveis de ambiente, atualiza arquivo config.json para config.js, cria o db finalmente, era pra ter feito no começo.

--- back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/controllers/bookController.js
+++ back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/controllers/bookController.js
@@ -1,11 +1,30 @@
-const bookService = require('../services/bookService');
+const BookService = require('../services/bookService');
 
 const getAll = async (_req, res) => {
-    const books = await bookService.getAll();
+    const books = await BookService.getAll();
 
     res.status(200).json(books);
 }
 
+const getById = async (req, res) => {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    res.status(200).json(book);
+  };
+
+const create = async (req, res) => {
+    const { title, author, pageQuantity } = req.body;
+
+    const newBook = await BookService.create({ title, author, pageQuantity });
+
+    res.status(201).json(newBook);
+}
+
 module.exports = {
     getAll,
+    getById,
+    create,
 }
--- back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/index.js
+++ back-end/bloco-24-ORM-Autenticacao/dia-01/exercicio-dia1/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bookController = require('./controllers/bookController');
+const BooksController = require('./controllers/bookController');
 
 app.use(express.json());
 
-app.get('/books', bookController.getAll);
+app.get('/books', BooksController.getAll);
+
+app.get('/books/:id', BooksController.getById);
+
+app.post('/books', BooksController.create);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
