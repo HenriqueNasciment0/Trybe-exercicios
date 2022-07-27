1e8155898d87c825d499ad80ce7f1388004075ed
HenriqueNasciment0 <you@example.com>
2022-07-27 13:35:34 -0300

    enum ts

---
+++ back-end/bloco-26-TypeScript/dia-01/testes/src/semana.ts
@@ -0,0 +1,31 @@
+enum semana {
+    segunda = 2,
+    terça = 3,
+    quarta = 4,
+    quinta = 5,
+    sexta = 6,
+    sábado = 7,
+    domingo = 1,
+}
+
+let dia: semana = semana.domingo;
+console.log(dia);
+
+enum arcoIris {
+    yellow = 'amarelo',
+    red = 'vermelho',
+    blue = 'azul',
+    green = 'verde',
+    orange = 'laranja',
+}
+
+let cor: arcoIris = arcoIris.blue;
+console.log(cor);
+
+enum acoes {
+    salvar = 1,
+    imprimir,
+    abrir,
+    visualizar,
+    fechar,
+}
