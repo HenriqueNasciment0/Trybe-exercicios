6dda67af4578b2f1fcb1b6f40e37a83e17285126
HenriqueNasciment0 <you@example.com>
2022-07-27 13:20:28 -0300

    iniciando exercicios ts

---
+++ back-end/bloco-26-TypeScript/dia-01/testes/src/teste.js
@@ -0,0 +1,12 @@
+var yes = true;
+var no = false;
+var zero = 0;
+var dez = 10;
+var cem = 100;
+var abc = 'abc';
+var nome = 'Henrique';
+function sayHelloWorld() {
+    console.log("Hello World!");
+}
+var nullValue = null;
+var undefinedValue = undefined;
