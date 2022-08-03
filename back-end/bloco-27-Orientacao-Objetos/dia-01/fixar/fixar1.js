99c8c9470f747a14e20e986657df4cfbf95f72dc
HenriqueNasciment0 <you@example.com>
2022-08-03 14:46:24 -0300

    testando chamadas privadas fora do escopo da class

--- back-end/bloco-27-Orientacao-Objetos/dia-01/fixar/fixar1.js
+++ back-end/bloco-27-Orientacao-Objetos/dia-01/fixar/fixar1.js
@@ -4,11 +4,30 @@ var Tv = /** @class */ (function () {
         this.size = size;
         this.resolution = resolution;
         this.connetions = connetions;
+        console.log(this);
     }
     Tv.prototype.turnOn = function () {
         console.log("Minha Tv nova \u00E9 da ".concat(this.brand, ", tem ").concat(this.size, ",\n         tem resolu\u00E7\u00E3o ").concat(this.resolution, " e conex\u00F5es ").concat(this.connetions));
     };
+    Object.defineProperty(Tv.prototype, "connectedTO", {
+        get: function () {
+            return this.connectedTo;
+        },
+        set: function (data) {
+            if (!data || this.connetions.includes(data)) {
+                this.connectedTo = data;
+                console.log(this.connectedTo);
+            }
+            else {
+                console.log('Sorry, connection unavailable!');
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Tv;
 }());
-var obj1 = new Tv('Panasonic', 60, '4k', 'USB, HDMI e Bluethoth');
+var obj1 = new Tv('Panasonic', 60, '4k', ['USB', 'HDMI', 'Bluethoth']);
 obj1.turnOn();
+obj1.connectedTO = 'USB';
+console.log(obj1.connectedTO);
--- back-end/bloco-27-Orientacao-Objetos/dia-01/fixar/fixar1.ts
+++ back-end/bloco-27-Orientacao-Objetos/dia-01/fixar/fixar1.ts
@@ -1,23 +1,40 @@
 class Tv {
-    brand: string;
-    size: number;
-    resolution: string;
-    connetions: string;
-    connectedTo?: string;
+    private brand: string;
+    private size: number;
+    private resolution: string;
+    private connetions: string[];
+    private connectedTo?: string;
 
-    constructor(brand: string, size: number, resolution: string, connetions: string) {
+    constructor(brand: string, size: number, resolution: string, connetions: string[]) {
         this.brand = brand;
         this.size = size;
         this.resolution = resolution;
         this.connetions = connetions;
+        console.log(this)
     }
 
     turnOn(): void {
         console.log(`Minha Tv nova é da ${this.brand}, tem ${this.size},
          tem resolução ${this.resolution} e conexões ${this.connetions}`)
     }
+
+    get connectedTO(): string | undefined {
+        return this.connectedTo;
+    }
+
+    set connectedTO(data: string | undefined) {
+        if (!data || this.connetions.includes(data)) {
+            this.connectedTo = data;
+            console.log(this.connectedTo);
+        } else {
+            console.log('Sorry, connection unavailable!')
+        }
+    }
 }
 
-const obj1 = new Tv('Panasonic', 60, '4k', 'USB, HDMI e Bluethoth');
+const obj1 = new Tv('Panasonic', 60, '4k', ['USB', 'HDMI', 'Bluethoth']);
 
 obj1.turnOn();
+
+obj1.connectedTO = 'USB';
+console.log(obj1.connectedTO);
