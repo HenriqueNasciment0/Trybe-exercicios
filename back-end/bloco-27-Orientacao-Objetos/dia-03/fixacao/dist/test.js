36a19b3fbc4efd4b43f2c630143fbe0d3bc20160
HenriqueNasciment0 <you@example.com>
2022-08-04 19:40:01 -0300

    segundo exercicio de fixacão do dia

--- back-end/bloco-27-Orientacao-Objetos/dia-03/fixacao/dist/test.js
+++ back-end/bloco-27-Orientacao-Objetos/dia-03/fixacao/dist/test.js
@@ -1,38 +1,35 @@
 "use strict";
-class Character {
-}
-class MeleeCharacter extends Character {
-    constructor(_name, _specialMove) {
-        super();
-        this._name = _name;
-        this._specialMove = _specialMove;
-    }
-    talk() {
-        console.log(`Sou ${this._name}, ataco de perto!`);
-    }
-    specialMove() {
-        console.log(`Tenho o poder de ${this._specialMove}`);
-    }
-}
-class LongRangeCharacter extends Character {
-    constructor(_name, _specialMove) {
-        super();
-        this._name = _name;
-        this._specialMove = _specialMove;
-    }
-    talk() {
-        console.log(`Sou ${this._name}, ataco de longe!`);
-    }
-    specialMove() {
-        console.log(`Tenho o poder de ${this._specialMove}`);
-    }
-}
-function showCharacter(character) {
-    character.talk();
-    character.specialMove();
-}
-;
-const personagem1 = new MeleeCharacter('Cobra', 'meter o dente envenenado!');
-const personagem2 = new LongRangeCharacter('Baleia', 'criar ondas afogadoras!');
-showCharacter(personagem1);
-showCharacter(personagem2);
+// abstract class Character {
+//     abstract talk(): void;
+//     abstract specialMove(): void;
+// }
+// class MeleeCharacter extends Character {
+//     constructor(private _name: string, private _specialMove: string) {
+//         super();
+//     }
+//     talk(): void {
+//         console.log(`Sou ${this._name}, ataco de perto!`)
+//     }
+//     specialMove(): void {
+//         console.log(`Tenho o poder de ${this._specialMove}`)
+//     }
+// }
+// class LongRangeCharacter extends Character {
+//     constructor(private _name: string, private _specialMove: string) {
+//         super();
+//     }
+//     talk(): void {
+//         console.log(`Sou ${this._name}, ataco de longe!`)
+//     }
+//     specialMove(): void {
+//         console.log(`Tenho o poder de ${this._specialMove}`)
+//     }
+// }
+// function showCharacter(character: Character) {
+//     character.talk();
+//     character.specialMove();
+// };
+// const personagem1 = new MeleeCharacter('Cobra', 'meter o dente envenenado!');
+// const personagem2 = new LongRangeCharacter('Baleia', 'criar ondas afogadoras!')
+// showCharacter(personagem1);
+// showCharacter(personagem2);
--- back-end/bloco-27-Orientacao-Objetos/dia-03/fixacao/test.ts
+++ back-end/bloco-27-Orientacao-Objetos/dia-03/fixacao/test.ts
@@ -1,46 +1,46 @@
-abstract class Character {
-    abstract talk(): void;
-    abstract specialMove(): void;
-}
+// abstract class Character {
+//     abstract talk(): void;
+//     abstract specialMove(): void;
+// }
 
-class MeleeCharacter extends Character {
+// class MeleeCharacter extends Character {
 
-    constructor(private _name: string, private _specialMove: string) {
-        super();
-    }
+//     constructor(private _name: string, private _specialMove: string) {
+//         super();
+//     }
 
-    talk(): void {
-        console.log(`Sou ${this._name}, ataco de perto!`)
-    }
+//     talk(): void {
+//         console.log(`Sou ${this._name}, ataco de perto!`)
+//     }
 
-    specialMove(): void {
-        console.log(`Tenho o poder de ${this._specialMove}`)
-    }
-}
+//     specialMove(): void {
+//         console.log(`Tenho o poder de ${this._specialMove}`)
+//     }
+// }
 
-class LongRangeCharacter extends Character {
+// class LongRangeCharacter extends Character {
 
-    constructor(private _name: string, private _specialMove: string) {
-        super();
-    }
+//     constructor(private _name: string, private _specialMove: string) {
+//         super();
+//     }
 
-    talk(): void {
-        console.log(`Sou ${this._name}, ataco de longe!`)
-    }
+//     talk(): void {
+//         console.log(`Sou ${this._name}, ataco de longe!`)
+//     }
 
-    specialMove(): void {
-        console.log(`Tenho o poder de ${this._specialMove}`)
-    }
-}
+//     specialMove(): void {
+//         console.log(`Tenho o poder de ${this._specialMove}`)
+//     }
+// }
 
-function showCharacter(character: Character) {
-    character.talk();
-    character.specialMove();
-};
+// function showCharacter(character: Character) {
+//     character.talk();
+//     character.specialMove();
+// };
 
-const personagem1 = new MeleeCharacter('Cobra', 'meter o dente envenenado!');
-const personagem2 = new LongRangeCharacter('Baleia', 'criar ondas afogadoras!')
+// const personagem1 = new MeleeCharacter('Cobra', 'meter o dente envenenado!');
+// const personagem2 = new LongRangeCharacter('Baleia', 'criar ondas afogadoras!')
 
-showCharacter(personagem1);
-showCharacter(personagem2);
+// showCharacter(personagem1);
+// showCharacter(personagem2);
 
