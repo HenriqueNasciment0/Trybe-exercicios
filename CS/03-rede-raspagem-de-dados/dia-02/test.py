37aed888c49c6d90be114d65a10d4bf285bff3dc
HenriqueNasciment0 <you@example.com>
2022-11-04 15:41:09 -0300

    requisição post

--- CS/03-rede-raspagem-de-dados/dia-02/test.py
+++ CS/03-rede-raspagem-de-dados/dia-02/test.py
@@ -1,6 +1,10 @@
 import requests
 
 response = requests.get("https://www.betrybe.com/")
-print(response.status_code)
-print(response.headers["Content-Type"])
+print("STATUS CODE: ", response.status_code)
+print("TIPO DO ARQUIVO: ", response.headers["Content-Type"])
 
+# print("HTML: ", response.text)
+
+response2 = requests.post("http://httpbin.org/post", data="some content")
+print(response2.text)
