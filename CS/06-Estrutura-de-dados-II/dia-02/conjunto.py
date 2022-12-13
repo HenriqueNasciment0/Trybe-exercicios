07fb047d3313437efe5b24ce4beb634f0c06ed08
HenriqueNasciment0 <you@example.com>
2022-12-13 18:02:07 -0300

    feat: metodo intersection

--- CS/06-Estrutura-de-dados-II/dia-02/conjunto.py
+++ CS/06-Estrutura-de-dados-II/dia-02/conjunto.py
@@ -33,6 +33,15 @@ class Conjunto:
 
         return new_conjunto
 
+    def intersection(self, conjuntoB):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.list[index] and conjuntoB.list[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
 
 if __name__ == "__main__":
     # conj = Conjunto()
@@ -42,14 +51,14 @@ if __name__ == "__main__":
     # print(conj)
 
     conj1 = Conjunto()
-    for i in range(1, 11):
+    for i in [1, 2, 3]:
         conj1.add(i)
 
     conj2 = Conjunto()
-    for i in range(10, 21):
+    for i in [7, 2, 10]:
         conj2.add(i)
 
-    conj3 = conj1.union(conj2)
+    conj3 = conj1.intersection(conj2)
     print(conj3)
 
 
